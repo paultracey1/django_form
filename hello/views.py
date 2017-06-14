8fd8c4a2629fb36cc7b5304c5b219f5ba4925725
Paul Tracey <Paul Tracey>
2017-06-14 12:23:18 +0100

    Can now add new contacts

--- hello/views.py
+++ hello/views.py
@@ -7,6 +7,14 @@ def say_hello(request):
 
 def add_contact(request):
     if request.method == "POST":
+        new = Contact()
+        new.first_name = request.POST.get('first_name')
+        new.last_name = request.POST.get('last_name')
+        new.mobile = request.POST.get('mobile')
+        new.email = request.POST.get('email')
+
+        new.save()
+        
         return render(request, 'contactadded.html')
     else:
         return render(request, 'add.html')
